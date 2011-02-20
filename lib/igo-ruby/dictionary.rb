cae4af064b7939aa8cd27c7d1f3ae2a94fdd6338
K.Nishi <24signals@gmail.com>
2011-02-21 06:08:18 +0900

    ファイルの移動/変名

---
+++ lib/igo-ruby/dictionary.rb
@@ -0,0 +1,12 @@
+# coding: utf-8
+#
+#= 形態素解析エンジンigo-ruby用の辞書ライブラリ
+#
+#Copyright:: Copyright (c) 2011, kyow
+#Authors:: K.Nishi
+#License:: MIT License ただし、内包する辞書のライセンスに関しては、辞書配布元のそれに準ずる
+
+module Igo
+  class Dictionary
+  end
+end
